e78f0c2b7c7ea9c3e7c35c2110010ce28130bf81
Vishnuvardhan reddy <b.reddy.vishnuvardhan@gmail.com>
2026-06-11 19:11:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 
-function toggleMenu(){
-  document.getElementById('menu').classList.toggle('show');
-}
-
 function scrollFilters(amount) {
     document.getElementById("filters")
         .scrollBy({ left: amount, behavior: "smooth" });
+}
+
+
+function toggleMenu() {
+
+    const menu = document.getElementById("menu");
+    const btn = document.getElementById("menuBtn");
+
+    menu.classList.toggle("show");
+
+    if(menu.classList.contains("show")){
+        btn.innerHTML = "✕";
+    } else {
+        btn.innerHTML = "☰";
+    }
+}
+
+function closeMenu(){
+
+    const menu = document.getElementById("menu");
+    const btn = document.getElementById("menuBtn");
+
+    menu.classList.remove("show");
+    btn.innerHTML = "☰";
 }
