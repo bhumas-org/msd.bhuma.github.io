393d1d0f3293e3a6d0f6b388637a5953180326fd
Vishnuvardhan reddy <b.reddy.vishnuvardhan@gmail.com>
2026-06-12 14:49:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-
-function scrollFilters(amount) {
-    document.getElementById("filters")
-        .scrollBy({ left: amount, behavior: "smooth" });
-}
-
-
 function toggleMenu() {
-
     const menu = document.getElementById("menu");
     const btn = document.getElementById("menuBtn");
 
     menu.classList.toggle("show");
+    btn.innerHTML = menu.classList.contains("show") ? "✕" : "☰";
+}
 
-    if(menu.classList.contains("show")){
-        btn.innerHTML = "✕";
-    } else {
-        btn.innerHTML = "☰";
+function closeMenu() {
+    document.getElementById("menu").classList.remove("show");
+    document.getElementById("menuBtn").innerHTML = "☰";
+}
+
+function openModal(src) {
+    document.getElementById("imageModal").style.display = "flex";
+    document.getElementById("modalImg").src = src;
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        closeModal();
     }
-}
+});
 
-function closeMenu(){
+document.querySelectorAll(".filters a").forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
 
-    const menu = document.getElementById("menu");
-    const btn = document.getElementById("menuBtn");
+        const target = document.querySelector(this.getAttribute("href"));
 
-    menu.classList.remove("show");
-    btn.innerHTML = "☰";
-}
+        const headerHeight = document.querySelector("header").offsetHeight;
+        const filterHeight = document.querySelector(".filter-wrapper").offsetHeight;
+
+        window.scrollTo({
+            top: target.offsetTop - headerHeight - filterHeight - 10,
+            behavior: "smooth"
+        });
+    });
+});
